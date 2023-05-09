61d6d487b208da25fa31121dc4c2352286717fb4
Antop90fr <antopfr90@gmail.com>
2023-05-09 16:22:00 +0200

    Added apples with create confectionery chocolates

--- src/main/java/net/antopfr/create_factory/item/ModItems.java
+++ src/main/java/net/antopfr/create_factory/item/ModItems.java
@@ -1,7 +1,9 @@
 package net.antopfr.create_factory.item;
 
+import com.mojang.blaze3d.shaders.Effect;
 import net.antopfr.create_factory.CreateFactory;
 import net.antopfr.create_factory.block.CreativeTab;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -34,6 +36,24 @@ public static final RegistryObject<Item> CHOCOLATE_APPLE = ITEMS.register("choco
 
 public static final RegistryObject<Item> HONEY_GLAZED_BERRIES = ITEMS.register("honey_glazed_berries",
         () -> new Item(new Item.Properties().tab(CreativeTab.CREATIVE_TAB_CF).food(new FoodProperties.Builder().nutrition(6).saturationMod(0.8f).build())));
+
+
+//Chocolate Apples
+public static final RegistryObject<Item> DARK_CHOCOLATE_APPLE = ITEMS.register("dark_chocolate_apple",
+        () -> new Item(new Item.Properties().tab(CreativeTab.CREATIVE_TAB_CF).food(new FoodProperties.Builder().nutrition(9).saturationMod(0.9f).build())));
+
+public static final RegistryObject<Item> WHITE_CHOCOLATE_APPLE = ITEMS.register("white_chocolate_apple",
+        () -> new Item(new Item.Properties().tab(CreativeTab.CREATIVE_TAB_CF).food(new FoodProperties.Builder().nutrition(9).saturationMod(0.9f).build())));
+
+public static final RegistryObject<Item> RUBY_CHOCOLATE_APPLE = ITEMS.register("ruby_chocolate_apple",
+        () -> new Item(new Item.Properties().tab(CreativeTab.CREATIVE_TAB_CF).food(new FoodProperties.Builder().nutrition(10).saturationMod(0.8f).build())));
+
+public static final RegistryObject<Item> CARAMEL_APPLE = ITEMS.register("caramel_apple",
+        () -> new Item(new Item.Properties().tab(CreativeTab.CREATIVE_TAB_CF).food(new FoodProperties.Builder().nutrition(9).saturationMod(0.6f).build())));
+
+
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
